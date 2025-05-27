63fa0f0045ac9b6313c73d3676ae0ce20e9b8c29
Kenzie Tandun <nudnateiznek@gmail.com>
2025-05-27 21:16:03 +1200

    use grid and templating for key projects page

--- app.py
+++ app.py
@@ -3,10 +3,35 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def render_to_file(source_filename, target_filename):
-    data = render_template(source_filename)
+def render_to_file(source_filename, target_filename, data=None):
+    data = render_template(source_filename, data=data)
     with open(target_filename, "w") as f:
         f.write(data)
+
+
+key_projects = [
+    {
+        "image": "images/optimized/hero-800.jpg",
+        "title": "Tarawera Landing",
+        "service": "Project management, civil engineering",
+        "location": "Hamilton",
+        "details": "Apex Civil in a joint venture with company name, constructed the new Hamilton Bridge",
+    },
+    {
+        "image": "images/optimized/hero-800.jpg",
+        "title": "Acacia Bay",
+        "service": "Project management, civil engineering",
+        "location": "Hamilton",
+        "details": "Apex Civil in a joint venture with company name, constructed the new Hamilton Bridge",
+    },
+    {
+        "image": "images/optimized/hero-800.jpg",
+        "title": "Marshall Road",
+        "service": "Project management, civil engineering",
+        "location": "Hamilton",
+        "details": "Apex Civil in a joint venture with company name, constructed the new Hamilton Bridge",
+    },
+]
 
 
 with app.app_context():
@@ -15,4 +40,4 @@ with app.app_context():
     render_to_file("our-services-civil-construction.html", "civil-construction.html")
     render_to_file("our-services-project-management.html", "project-management.html")
     render_to_file("our-services-land-development.html", "land-development.html")
-    render_to_file("key-projects.html", "key-projects.html")
+    render_to_file("key-projects.html", "key-projects.html", data=key_projects)
