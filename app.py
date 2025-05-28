8250e7ff84ddc61706ec764ae45dab896b29acd7
Kenzie Tandun <nudnateiznek@gmail.com>
2025-05-28 20:20:39 +1200

    wip key project details page

--- app.py
+++ app.py
@@ -11,25 +11,34 @@ def render_to_file(source_filename, target_filename, data=None):
 
 key_projects = [
     {
+        "html": "tarawera-landing.html",
         "image": "images/optimized/hero-800.jpg",
         "title": "Tarawera Landing",
         "service": "Project management, civil engineering",
         "location": "Hamilton",
         "details": "Apex Civil in a joint venture with company name, constructed the new Hamilton Bridge",
+        "client": "We Build NZ",
+        "more_details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     },
     {
+        "html": "acacia-bay.html",
         "image": "images/optimized/hero-800.jpg",
         "title": "Acacia Bay",
         "service": "Project management, civil engineering",
         "location": "Hamilton",
         "details": "Apex Civil in a joint venture with company name, constructed the new Hamilton Bridge",
+        "client": "We Build NZ",
+        "more_details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     },
     {
+        "html": "marshall-road.html",
         "image": "images/optimized/hero-800.jpg",
         "title": "Marshall Road",
         "service": "Project management, civil engineering",
         "location": "Hamilton",
         "details": "Apex Civil in a joint venture with company name, constructed the new Hamilton Bridge",
+        "client": "We Build NZ",
+        "more_details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     },
 ]
 
@@ -41,3 +50,10 @@ with app.app_context():
     render_to_file("our-services-project-management.html", "project-management.html")
     render_to_file("our-services-land-development.html", "land-development.html")
     render_to_file("key-projects.html", "key-projects.html", data=key_projects)
+
+    for project in key_projects:
+        render_to_file(
+            "key-project-details.html",
+            project["html"],
+            data={"project": project, "other_projects": [k for k in key_projects if k["title"] != project["title"]]},
+        )
--- templates/navbar.js
+++ templates/navbar.js
@@ -1,7 +1,7 @@
-      function toggleNav() {
-        const navLinksMobile = document.querySelector(".nav-links-mobile");
-        navLinksMobile.classList.toggle("open");
+function toggleNav() {
+  const navLinksMobile = document.querySelector(".nav-links-mobile");
+  navLinksMobile.classList.toggle("open");
 
-        const topNav = document.querySelector(".navbar");
-        topNav.classList.toggle("open");
-      }
+  const topNav = document.querySelector(".navbar");
+  topNav.classList.toggle("open");
+}
